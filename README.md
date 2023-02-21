fba589023e5e186a4b79bb18af5b3bc138fa4d3f
D.J. Crosby <taladan@gmail.com>
2023-02-20 21:19:39 -0500

    Finalize game logic and style for play
    Big commit here:
    
      - Add images for player interaction
      - Style and layout of complete page
      - refactor 'button' logic to use figure images with captions
        (Mostly for alignment)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Aim
 
 To create a Rock/Paper/Scissors game as assigned by [The Odin Project](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors)
+
+Updated with a functioning GUI and display of output and win states.
--- scripts/rps.js
+++ scripts/rps.js
@@ -14,25 +14,53 @@ computerWins = currentScores[2];
 const btnContainer = document.getElementById("btns");
 
 // create three buttons
-const rock = document.createElement("button");
-const paper = document.createElement("button");
-const scissors = document.createElement("button");
+// const rock = document.createElement("button");
+// const paper = document.createElement("button");
+// const scissors = document.createElement("button");
+const rock = document.createElement("figure");
+const rockImg = document.createElement("img");
+const rockCaption = document.createElement("figcaption");
+const rockText = document.createTextNode("Rock");
+rockImg.id = "rock";
+rockImg.setAttribute("src", "../img/rock.png");
 
-// game choices
-const choices = Array("rock", "paper", "scissors");
+const paper = document.createElement("figure");
+const paperImg = document.createElement("img");
+const paperCaption = document.createElement("figcaption");
+const paperText = document.createTextNode("Paper");
+paperImg.id = "paper";
+paperImg.setAttribute("src", "../img/paper.png");
+
+const scissors = document.createElement("figure");
+const scissorsImg = document.createElement("img");
+const scissorsCaption = document.createElement("figcaption");
+const scissorsText = document.createTextNode("Scissors");
+scissorsImg.id = "scissors";
+scissorsImg.setAttribute("src", "../img/scissors.png");
+
+rockCaption.className = "caption";
+paperCaption.className = "caption";
+scissorsCaption.className = "caption";
 
 // add button texts
-rock.textContent = "Rock";
-rock.id = "rock";
-paper.textContent = "Paper";
-paper.id = "paper";
-scissors.textContent = "Scissors";
-scissors.id = "scissors";
+rockCaption.appendChild(rockText);
+paperCaption.appendChild(paperText);
+scissorsCaption.appendChild(scissorsText);
+
+rock.appendChild(rockImg);
+rock.appendChild(rockCaption);
+paper.appendChild(paperImg);
+paper.appendChild(paperCaption);
+scissors.appendChild(scissorsImg);
+scissors.appendChild(scissorsCaption);
 
 // add buttons to container
 btnContainer.appendChild(rock);
 btnContainer.appendChild(paper);
 btnContainer.appendChild(scissors);
+
+// game choices
+const choices = Array("rock", "paper", "scissors");
 
 // Add even listeners to buttons
 Array.from(btnContainer.children).forEach(function (btn) {
@@ -81,9 +109,13 @@ function compareChoices(computerChoice, playerChoice) {
 
 // game logic
 function play(e) {
+  //clear text shadow if it exists
+  bumper.style.textShadow = "";
+
   const computerChoice = getComputerChoice(choices);
   const playerChoice = e.target.id;
   const outcome = compareChoices(computerChoice, playerChoice);
+  console.log(computerChoice);
 
   // Determine outcome
   switch (outcome) {
@@ -91,17 +123,21 @@ function play(e) {
       ties = ties + 1;
       currentScores[1] = ties;
       updateScoreDisplay();
+      bumper.textContent = "The computer chose " + computerChoice.toUpperCase();
       break;
     case "player":
       playerWins = playerWins + 1;
       currentScores[0] = playerWins;
       updateScoreDisplay();
+      bumper.textContent = "The computer chose " + computerChoice.toUpperCase();
       // console.log("Player Wins: " + playerWins);
       if (playerWins === 5 && computerWins < 5) {
         // set player win state
         // console.log("Player Wins!");
         currentScores = resetScores();
         bumper.textContent = "You won the last battle!";
+        bumper.style.textShadow =
+          "2px 8px 6px rgba(0,0,0,0.2), 0px -5px 35px rgba(255,255,255,0.3)";
         updateScoreDisplay();
       }
       break;
@@ -109,12 +145,16 @@ function play(e) {
       computerWins = computerWins + 1;
       currentScores[2] = computerWins;
       updateScoreDisplay();
+      bumper.textContent = "The computer chose " + computerChoice.toUpperCase();
       // console.log("Computer Wins: " + computerWins);
       if (playerWins < 5 && computerWins === 5) {
         // set computer win state
         // console.log("Computer Wins!");
         currentScores = resetScores();
         bumper.textContent = "The Computer won the last battle!";
+        bumper.style.textShadow =
+          "2px 8px 6px rgba(0,0,0,0.2), 0px -5px 35px rgba(255,255,255,0.3)";
+
         updateScoreDisplay();
       }
       break;
@@ -149,14 +189,22 @@ const body = document.body;
 
 // h1
 const title = document.getElementById("head-title");
+rules = document.createElement("div");
+rules.textContent = "(First to 5 wins)";
+rules.id = "rules";
+title.appendChild(rules);
 
 // titleDiv mods
 const scoreBoard = document.createElement("div");
-const bumper = document.createTextNode(
+const bumper = document.createElement("div");
+const bumperText = document.createTextNode(
   "Click 'Rock', 'Paper' or 'Scissors' to play!"
 );
+bumper.id = "bumper";
+bumper.appendChild(bumperText);
 scoreBoard.id = "scoreBoard";
 scoreBoard.className = "score";
+scoreBoard.appendChild(bumper);
 
 // scoreboard displays as a table
 let scoreTable = document.createElement("table");
